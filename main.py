279e6d8a4930583896e9f5cdb506fa3c71536155
mateusz <mateusz.basiak@gmail.com>
2021-03-14 22:04:57 +0100

    playable game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 
 from pygame.locals import (
+    RLEACCEL,
     K_UP,
     K_DOWN,
     K_LEFT,
@@ -10,19 +12,116 @@ from pygame.locals import (
     QUIT
 )
 
-pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+
+class Enemy(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("missle.png")
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+        self.speed = random.randint(2, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load("jet.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
+pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+# screen.fill((255, 255, 255))
+# pygame.display.flip()
+player = Player()
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+clock = pygame.time.Clock()
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-
         elif event.type == QUIT:
             running = False
+
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+        # Get all the keys currently pressed
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+    enemies.update()
+    # screen.fill((255, 255, 255))
+    # surf = pygame.Surface((50, 50))
+    screen.fill((0, 0, 0))
+
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+        running = False
+    # rect = surf.get_rect()
+    surf_center = (
+
+        (SCREEN_WIDTH - player.surf.get_width()) / 2,
+
+        (SCREEN_HEIGHT - player.surf.get_height()) / 2
+
+    )  # Draw surf at the new coordinates
+
+    # screen.blit(player.surf, surf_center)
+    screen.blit(player.surf, player.rect)
+
+    pygame.display.flip()
+    clock.tick(30)
